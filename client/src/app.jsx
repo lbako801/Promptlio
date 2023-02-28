2f16a9f1f883a2670e67d2191d1d4d60485276af
lbako801 <lbako98@gmail.com>
2023-02-28 12:48:23 -0700

    made client react-app. Made inital src components and some little css variables and what not

---
+++ client/src/app.jsx
@@ -0,0 +1,10 @@
+// I made all components a .jsx instead of a .js file. This is so we can use the 'rafce' module and make it easier to get started for each component!
+import React from 'react'
+
+const app = () => {
+  return (
+    <div><h1>👨‍👩‍👦‍👦 Hey look! It's us! #stackedteam</h1></div>
+  )
+}
+
+export default app
