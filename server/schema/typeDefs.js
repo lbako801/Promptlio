d41b1cfef17cb73d7c38ef07899d7b0f89dc2a33
Ari N <an-nielson@hotmail.com>
2023-02-28 21:08:15 -0700

    updated typeDefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -45,12 +45,16 @@ const typeDefs = graphQL`
     getUsers: User
     posts: [Post]
     post(id: Int!): Post
+    comment(post: ID): Comment
+    prompts: Prompt
   }
 
   type Mutation {
     login(username: String!, password: String!): Auth
     register(name: String!, username: String!, email: String!, password: String!): Auth
-    createPost(post: String!, creator: String!, likes: Int!, createdAt: String!, comment: String!):
+    createPost(post: String!, creator: String!, likes: Int!, createdAt: String!, comment: String!): Post
+    addComment(post: ID!, creator: String!, text: String!, created_at: String!): Comment
+    newPrompt(title: String!, ): Prompt
   }
 
   `
