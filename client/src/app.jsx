8fe151a4ba18b3ece0c74d736de7ecf2422752cc
lbako801 <lbako98@gmail.com>
2023-02-28 19:10:56 -0700

    initial react boilerplate

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,12 @@
 // I made all components a .jsx instead of a .js file. This is so we can use the 'rafce' module and make it easier to get started for each component!
 import React from 'react'
+import Nav from './components/nav/nav'
 
 const app = () => {
   return (
-    <div><h1>👨‍👩‍👦‍👦 Hey look! It's us! #stackedteam</h1></div>
+    <div>
+      <Nav />
+    </div>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 dbConnection.once('open', () => {
     app.listen(PORT, () => {
         console.log(`App running on port localhost:${PORT}!`);
