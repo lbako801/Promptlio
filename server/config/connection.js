7b8e44c340c09f8db41b8e4dd8aaa02bd4feb555
Jace Edwards <jaceedwards@Jace-Edwards-MBP32-2331.local>
2023-03-08 21:51:04 -0700

    require dotenv config in config/connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+console.log(process.env.MONGODB_URI);
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/promptlio",
